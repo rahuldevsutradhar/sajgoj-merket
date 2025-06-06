bf51f03740098740d3a02775744fd6acd9f5ae16
rahuldevsutradhar <rahulkumar016332@gmail.com>
2025-06-06 11:55:02 +0600

    add error page

--- src/assets/pages/Error.jsx
+++ src/assets/pages/Error.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Container from '../component/Container'
+import Flex from '../component/Flex'
+import Image from '../component/Image'
+import ButtonOne from '../component/ButtonOne'
+import error from '../404.webp'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <section>
+      <Container>
+        <Flex className='items-center '>
+          <Image src={error}/>
+          <div>
+            <h1 className='font-bold font-mon text-[32px]'>UH OH! You are lost.</h1>
+            <p className='font-mon mb-3 text-[#b0b2b3]'>The page you are looking for does not exist. Click the button below to go back to the homepage.</p>
+            <Flex className='gap-x-4'>
+              <Link to={'/'}><ButtonOne text='Home'
+              className='border-4 border-one  text-black hover:bg-one hover:text-white duration-500'/></Link>
+              <Link to={'/shop'}><ButtonOne text='Shop'
+              className='border-4 border-black  text-black hover:bg-black hover:text-white duration-500'/></Link>
+            </Flex>
+          </div>
+
+        </Flex>
+      </Container>
+    </section>
   )
 }
 
