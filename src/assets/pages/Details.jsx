ba3d7506eb35e4f788707089fe827155ead41b36
rahuldevsutradhar <rahulkumar016332@gmail.com>
2025-06-08 09:37:27 +0600

    add responsive details page

--- src/assets/pages/Details.jsx
+++ src/assets/pages/Details.jsx
@@ -16,26 +16,26 @@ const Details = () => {
   const decrement = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
 
   return (
-    <section className='my-[70px]'>
+    <section className='my-[15px] md:my-[70px]'>
       <Container>
-        <Flex>
-          <div className='w-7/12'>
+        <Flex className='md:flex-row flex-col'>
+          <div className='w-auto sm:w-100% md:w-7/12 p-2 sm:p-0  md:p-0 '>
             <ProductGallery />
-            <ul className='flex w-[30px] h-[30px] gap-x-4 mx-auto mt-5 '>
+            <ul className='flex w-[30px] h-[30px] gap-x-4 mx-auto mt-5  '>
               <li><FaFacebook /></li>
               <li><FaLinkedin /> </li>
               <li><IoLogoWhatsapp /> </li>
             </ul>
-            <h2 className="text-md font-mon font-semibold mb-4">Frequently Bought Together</h2>
-            <div className='flex gap-x-4'>
-              <div className='w-1/2 flex justify-between items-center'>
+            <h2 className="text-md sm:px-2 px-0 font-mon font-semibold mb-4">Frequently Bought Together</h2>
+            <div className='flex-col flex sm:flex-row md:flex-row gap-x-4'>
+              <div className='w-auto md:w-1/2 flex justify-between items-center px-0 sm:px-2 pr-2 md:pr-0 '>
                 <Image src={img1} className='w-[130px] h-[130px]' />
-                <span className='text-3xl'>+</span>
+                <span className='text-3xl pl-2 md:pl-0'>+</span>
                 <Image src={img2} className='w-[130px] h-[130px]' />
               </div>
-              <div className='w-1/2 mx-auto flex flex-col items-center top-60% '>
+              <div className='w-auto md:w-1/2 mx-auto flex flex-col items-center top-60% '>
                 <div className="mt-2 text-sm font-medium text-pink-600 mx-auto">
-                  <span className='font-mon text-sm text-black'>Total price</span>
+                  <span className='font-mon pb-2 text-sm text-black'>Total price</span>
                   ৳ 139.00
                 </div>
                 <ButtonOne text='Add to card' className='bg-one inline-block text-white ' />
@@ -45,13 +45,13 @@ const Details = () => {
             </div>
           </div>
 
-          <div className='w-5/12'>
+          <div className='w-auto md:w-5/12 px-2 md:px-0 pt-0 sm:pt-8'>
             <h1 className='font-mon text-lg font-normal leading-[20px]'>
               Himalaya Brightening Vitamin C Orange Face Wash-100ml
             </h1>
 
 
-            <Flex className='gap-x-3 items-center'>
+            <Flex className='gap-x-9 md:gap-x-3 items-center pt-0 sm:pt-4 '>
               <h1 className='text-sm text-gray-500 mt-2'>Size: 100ml</h1>
               <div className='flex mt-2 items-center bg-green-600 text-white gap-1 px-2 py-[2px] rounded-full text-sm font-medium'>
                 <p>4.3★ | 3 Reviews</p>
@@ -59,7 +59,7 @@ const Details = () => {
 
             </Flex>
 
-            <div className='flex items-center gap-4 flex-wrap mt-2'>
+            <div className='flex items-center gap-y-2 md:gap-y-0 gap-x-8 md:gap-x-4 flex-wrap mt-2'>
               <span className='font-mon text-one font-bold'>৳119.00</span>
               <span className='font-mon text-[#d9dade]'><del>৳200.00</del></span>
               <span className='font-mon text-[#d9dade]'> | </span>
@@ -84,7 +84,7 @@ const Details = () => {
 
 
 
-            <div className='flex items-center gap-x-4 mb-4'>
+            <div className='flex items-center gap-x-8 md:gap-x-4 mb-4'>
               <FaHeart className='text-white bg-black py-2 px-2 w-[30px] h-[30px] rounded-lg' />
               <div className='border rounded-lg flex'>
                 <ButtonOne onClick={increment} text='-' className='px-3 py-2 text-lg font-bold' />
@@ -96,9 +96,9 @@ const Details = () => {
             <p className='text-sm font-mon mb-6'>Only <span className='text-one'>1</span> item left in stock</p>
 
             <div className='flex gap-x-6 text-mon items-top text-five'>
-              <h2 className="text-lg text-md mb-2">Brief Description</h2>
+              <h2 className="text-base md:text-lg pr-12 md:pr-0 font-semibold mb-2 sm:mb-0">Brief Description</h2>
               <div className='mb-6'>
-                <ul className="list-disc list-inside text-xs gap-y-2 flex flex-col">
+                <ul className="list-disc list-inside text-base md:text-xs gap-y-2 flex flex-col">
                   <li>Increase hair growth.</li>
                   <li>Provide essential Vitamin E to the hair.</li>
                   <li>Comes with nice fragrance.</li>
@@ -158,12 +158,12 @@ const Details = () => {
 
           </div>
         </Flex>
-        <div className="mt-5 text-sm text-gray-700">
+        <div className="px-2 md:px-0 mt-5 text-sm text-gray-700">
           ✅ This Item: Parachute Advansed Onion Enriched Coconut Hair Growth Oil (200ml)
           <del><span className='text-one'>৳ 215.00</span></del>
           <span className='text-one'>৳ 139.00</span>
         </div>
-        <div className="mt-5 text-sm text-gray-700 w-1/2">
+        <div className="px-2 md:px-0 mt-5 text-sm text-gray-700 w-auto md:w-1/2">
           ✅ This Item:
           Parachute Naturale Nourishing Care Shampoo with Aloe Vera & Coconut Milk, For Strong & Silky Hair, Smoothens Hair, Paraben Free, 100% Vegan, All Hair Types, 160 ml
           (160ml)
@@ -171,14 +171,14 @@ const Details = () => {
           <span className='text-one'>৳ 99.00</span>
 
         </div>
-
-        <div className='mt-8 flex justify-between px-70'>
-          <ButtonOne text='Description' className='bg-[#e5e7ef] text-black inline-block hover:text-white hover:bg-one duration-500'/>
-          <ButtonOne text='Ingredients' className='bg-[#e5e7ef] text-black inline-block hover:text-white hover:bg-one duration-500'/>
-          <ButtonOne text='How To Use' className='bg-[#e5e7ef] text-black inline-block hover:text-white hover:bg-one duration-500'/>
-          <ButtonOne text='Reviews (64)' className='bg-[#e5e7ef] text-black inline-block hover:text-white hover:bg-one duration-500'/>
-          <ButtonOne text='Q&A (17)' className='bg-[#e5e7ef] text-black inline-block hover:text-white hover:bg-one duration-500'/>
+        <div className="mt-8 flex justify-between px-2 md:px-70 ">
+          <ButtonOne text="Description" className="bg-[#e5e7ef] text-black hover:text-white hover:bg-one duration-500" />
+          <ButtonOne text="Ingredients" className="hidden sm:inline-block md:inline-block bg-[#e5e7ef] text-black hover:text-white hover:bg-one duration-500" />
+          <ButtonOne text="How To Use" className="hidden sm:inline-block md:inline-block bg-[#e5e7ef] text-black hover:text-white hover:bg-one duration-500" />
+          <ButtonOne text="Reviews (64)" className="bg-[#e5e7ef] text-black hover:text-white hover:bg-one duration-500" />
+          <ButtonOne text="Q&A (17)" className="bg-[#e5e7ef] text-black hover:text-white hover:bg-one duration-500" />
         </div>
+
       </Container>
     </section>
   );
